7235e336fa8d07c206d11e740771538751eef7f4
Nikita Nikiforov <nikiforovpizza@gmail.com>
2020-03-23 21:33:06 +0200

    Clean styles, update data

--- src/data/pictures.js
+++ src/data/pictures.js
@@ -2,22 +2,22 @@ const pictures = [
     {
         src: "/pictures/1.jpg",
         name: "Picture 1",
-        material: "Material 1",
-        size: "Size 1",
+        material: "plastic bags LDPE4/HDPE2",
+        size: "120х120cm",
         price: "100$"
     },
     {
         src: "/pictures/2.jpg",
-        name: "Picture 1",
-        material: "Material 2",
-        size: "Size 1",
+        name: "Picture 2",
+        material: "plastic bags LDPE4/HDPE2",
+        size: "120x80cm",
         price: "400$"
     },
     {
         src: "/pictures/3.jpg",
-        name: "Picture 1",
+        name: "Picture 3",
         material: "plastic bags LDPE4/HDPE2",
-        size: "Size 3",
+        size: "120x80cm",
         price: "500$"
     }
 ];
